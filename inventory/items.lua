ee71a7160b52b3a64c3099f7e846a0cd61295d77
xKurizu <139591961+xKurizuSan@users.noreply.github.com>
2023-10-25 15:54:59 +0200

    Add files via upload

---
+++ inventory/items.lua
@@ -0,0 +1,34 @@
+["plastic"] = {
+	label = "Plastic",
+	weight = 1,
+	stack = true,
+	close = true,
+},
+
+["glass"] = {
+	label = "Glass",
+	weight = 1,
+	stack = true,
+	close = true,
+},
+
+["rubber"] = {
+	label = "Rubber",
+	weight = 1,
+	stack = true,
+	close = true,
+},
+
+["aluminum"] = {
+	label = "Aluminum",
+	weight = 1,
+	stack = true,
+	close = true,
+},
+
+["metalscrap"] = {
+	label = "Metal Scrap",
+	weight = 1,
+	stack = true,
+	close = true,
+},
